834cb2790fb430befe24cdd4d4d03fcdea3cf620
ma-santoso <marsa.agung.s@mail.ugm.ac.id>
2016-09-19 14:27:08 +0700

    Implemented nativeMessaging API

--- app/js/background.js
+++ app/js/background.js
@@ -1,5 +1,4 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.runtime.connectNative('terminal');
   chrome.app.window.create('../window.html', {
   	id: 'main',
     'outerBounds': {
